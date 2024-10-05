c852482a0d5a22af7ff1c16ed8fddfe8083936ce
MattyMyers22 <mmyers2206@gmail.com>
2024-10-05 11:42:15 -0700

    initial commit for scripts directory and extract.py

---
+++ scripts/extract.py
@@ -0,0 +1,7 @@
+# Collect all data sources for training and testing into their own 
+# respective pandas DataFrames
+
+import pandas as pd
+import os
+
+lunar_cat_directory = './data/lunar/training/catalogs/'
