df013b330b3eb07fc3833347e86ad18ac1f1ede5
MattyMyers22 <mmyers2206@gmail.com>
2024-10-06 16:45:16 -0700

    initial commit of mars_model.py

---
+++ scripts/mars_model.py
@@ -0,0 +1,71 @@
+# Provide a sample Random Forest ML Model for seismic event detection on Mars
+import pandas as pd
+import numpy as np
+import os
+import matplotlib.pyplot as plt
+from obspy import read
+from obspy.signal.invsim import cosine_taper
+from obspy.signal.filter import highpass
+from obspy.signal.trigger import classic_sta_lta, plot_trigger, trigger_onset
+
+# Reading mseed training data
+training_directory = './data/mars/training/data/'
+
+data = []
+
+for file in os.listdir(training_directory):
+    if file.endswith('.mseed'):
+        st = read(f'{training_directory}{file}')
+        data.append(st)
+
+print(data)
+
+"""
+
+# Mars window lengths
+mars_short = 100
+mars_long = 500
+
+# Calculate Mars sampling frequency
+mars_obs = len(mars_train)
+mars_time_diffs = mars_train.groupby('filename')['time_abs'].apply(
+    lambda x: (x.max() - x.min()).total_seconds()
+)
+mars_total_time = mars_time_diffs.sum()
+mars_sampling_freq = (mars_obs / mars_total_time)
+
+# Get numpy array of velocity observations
+mars_tr_data = mars_train['velocity(c/s)'].to_numpy()
+
+# Get numpy array of time observations
+mars_tr_times = mars_train['time_abs'].to_numpy()
+
+# Run Obspy's STA/LTA to obtain a characteristic function
+mars_cft = classic_sta_lta(mars_tr_data, int(mars_short * mars_sampling_freq), int(mars_long * mars_sampling_freq))
+
+# Plot characteristic function
+fig,ax = plt.subplots(1,1,figsize=(12,3))
+ax.plot(mars_tr_times,mars_cft)
+ax.set_xlim([min(mars_tr_times),pd.Timestamp('2022-01-03 00:00:00')])
+ax.set_xlabel('Time')
+ax.set_ylabel('Characteristic function')
+plt.show()
+
+# Mars triggers
+mars_thr_on = 2.5
+mars_thr_off = 1.0
+on_off = np.array(trigger_onset(mars_cft, mars_thr_on, mars_thr_off))
+
+# Plot on and off triggers
+fig,ax = plt.subplots(1,1,figsize=(12,3))
+for i in np.arange(0,len(on_off)):
+    triggers = on_off[i]
+    ax.axvline(x = mars_tr_times[triggers[0]], color='red', label='Trig. On')
+    ax.axvline(x = mars_tr_times[triggers[1]], color='purple', label='Trig. Off')
+
+# Plot seismogram
+ax.plot(mars_tr_times,mars_tr_data)
+ax.set_xlim([min(mars_tr_times),max(mars_tr_times)])
+ax.legend()
+plt.show()
+"""
--- scripts/testing.py
+++
@@ -1,7 +0,0 @@
-import extract
-
-lunar_train = extract.extract_data('lunar', 'train', 'csv')
-lunar_catalog = extract.extract_data('lunar', 'catalog', 'csv')
-mars_train = extract.extract_data('mars', 'train', 'csv')
-mars_catalog = extract.extract_data('mars', 'catalog', 'csv')
-mars_test = extract.extract_data('mars', 'test', 'csv')
